fac1a4040cd1452ff77918f5afdf51b3ef025480
OhManLolLol <32421828+OhManLolLol@users.noreply.github.com>
2021-10-30 13:31:43 -0400

    changing basic info

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "BSED Tree",
+	id: "mymodlol",
+	author: "OhMan",
+	pointsName: "Cash",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
